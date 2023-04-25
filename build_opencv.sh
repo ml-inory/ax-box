48e10fa0ad56bd911a2f3fba28029cb36c944666
祈Inory <584688538@qq.com>
2023-04-25 17:12:58 +0800

    add ffmpeg opencv

--- build_opencv.sh
+++ build_opencv.sh
@@ -11,7 +11,7 @@ git clone https://github.com/opencv/opencv.git opencv -b 4.3.0 --depth=1
 cd opencv
 mkdir -p build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=./install -DWITH_IPP=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_DOCS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_opencv_world=ON
+cmake .. -DCMAKE_INSTALL_PREFIX=./install -DWITH_IPP=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_DOCS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_opencv_apps=OFF -DBUILD_opencv_world=ON
 make -j8
 make install
 
